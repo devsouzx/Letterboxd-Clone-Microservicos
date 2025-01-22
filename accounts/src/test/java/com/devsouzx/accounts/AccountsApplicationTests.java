72b225209e4daa38d89520e031b6a578242ead0b
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-01-21 21:31:26 -0300

    initialize accounts service

---
+++ accounts/src/test/java/com/devsouzx/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsouzx.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
