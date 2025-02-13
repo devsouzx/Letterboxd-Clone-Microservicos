9e8219b762173c42ed23775218281b5b0410a106
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-02-13 12:12:18 -0300

    Film microservice: first commit

---
+++ Film/src/test/java/com/devsouzx/Film/FilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsouzx.Film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
